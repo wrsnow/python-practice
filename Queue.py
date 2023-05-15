ba5734c44ac8cac83b1dd680bfa00ef0a4296766
wrsnow <wesleyrsnow@gmail.com>
2023-05-15 02:12:15 -0300

    refactor:queue
    Add return messages to queue methods and add queue max capacity

--- Queue.py
+++ Queue.py
@@ -2,32 +2,33 @@ import random
 
 
 class Queue:
+    '''
+    DASDASDASSDASDASD
+    '''
     items = None
     start = None
     end = None
+    max = None
 
     def __init__(self):
         self.items = []
         self.start = 0
         self.end = 0
+        self.max = 10;
 
-    def enqueue(self, element) -> None:
+    def enqueue(self, element):
+      if len(self.items) < self.max:
         self.items.append(element)
         self.end += 1
-        print("")
+        return "Added to queue"
+      else:
+        return "Queue is full"
 
     def dequeue(self):
         if self.is_empty():
             return "Queue is empty"
         else:
-            tempArr = []
-            for i in range(0, len(self.items)):
-                if i == 0:
-                    pass
-                else:
-                    tempArr.append(self.items[i])
-            self.items = tempArr
-            self.end -= 1
+            self.items.pop(0)
             return "Item poped"
 
     def is_empty(self):
@@ -48,8 +49,11 @@ class Queue:
 
 
 queue = Queue()
+for i in range(0, 14):
+    print(queue.enqueue(random.randint(0, 100)))
+
 for i in range(0, 10):
-    queue.enqueue(random.randint(0, 100))
-for i in range(0, 10):
+    print(queue)
     print(queue.dequeue())
-    print(queue.peek())
+
+print(queue.peek())
