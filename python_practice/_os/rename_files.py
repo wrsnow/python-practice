1d376433b04bb1ae3a6a2bb22be9c6c8307bac1e
wrsnow <wesleyrsnow@gmail.com>
2023-05-18 21:31:45 -0300

    fix: rename_files.py
    Fix bug where the script would take all the content after finding a dot and use as the file extension

--- python_practice/_os/rename_files.py
+++ python_practice/_os/rename_files.py
@@ -5,8 +5,6 @@ import re
 ## List all content inside the folder
 dirfiles = os.listdir()
 
-def is_file(string):
-	return bool(re.search(r"\..*\w", string))
 
 def rename_file(filename:str,new_name:str):
 	try:
@@ -16,24 +14,23 @@ def rename_file(filename:str,new_name:str):
 
 ##
 def renameMany(files:list, basename:str)->None:
-  print("[app] started...")
-  print("[app] renaming files...")
-  counter = 0
-  for file in files:
-    counter+= 1
-    file_ext = re.findall(r"\..*\w",file)[-1]
-    rename_file(file, f"{basename} {counter}{file_ext}")
-  print("[app] finished")
+	print("[app] started...")
+	print("[app] renaming files...")
+	counter = 0
+	for file in files:
+		counter+= 1
+		file_ext = file.split(".")[-1]
+		rename_file(file, f"{basename} {counter}.{file_ext}")
+	print("[app] finished")
 
 ## filter the content of the folder and store only files
 files = []
 
 ## check if it a file or a folder and add to files List
 for file in dirfiles:
-  if is_file(file):
+  if os.path.isfile(file):
     files.append(file)
 
 ## prompt the user to choose a name for the renamed files
 desired_output = input("Output name: ")
-
 renameMany(files, desired_output)
